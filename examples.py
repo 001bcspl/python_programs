75b7fae45f704b461ad3182c78578f7ab6cf58a6
Bhavani H B <your-email@example.com>
2025-06-09 08:01:03 +0000

    second commit

--- examples.py
+++ examples.py
@@ -1,2 +1,25 @@
 print("Hello World")
 print("I am Bhavani H B")
+
+
+
+
+name = "Bhavani"
+age = 22
+height = 5.1
+is_student = True
+student_info = {"name": "Bhavani", "age": 22, "is_student": True}
+
+#To Print the variables
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print(f"Is student: {is_student}")
+print(f"Student Info: {student_info}")
+
+# To the Check data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(student_info))
